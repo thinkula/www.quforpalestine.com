79dfbd446b4b73095b778601cfc5ecccefb5c3cc
Joe Ugalde <thinkula@gmail.com>
2024-06-09 23:50:13 -0400

    adding php form

---
+++ submit_form.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Define the recipient email address
+    $to = "qualumniforpalestine@protonmail.com";  // Change this to your desired email address
+    $subject = "New Contact Form Submission from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "X-Mailer: PHP/" . phpversion();
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
