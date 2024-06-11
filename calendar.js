1d44ae268301469e63368e69040ce3a3e7fc820f
Joe Ugalde <thinkula@gmail.com>
2024-06-11 00:20:03 -0400

    calendar update

--- calendar.js
+++ calendar.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     const calendar = document.getElementById("calendar");
 
     // Get the current date
